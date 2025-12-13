dc24bb777d602a9dbf000c95aa35fee15fa1e1db
Roman Kvas <rk.sambir@gmail.com>
2025-12-13 15:07:11 +0200

    fix app.js

--- app.js
+++ app.js
@@ -5,15 +5,12 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const cors = require('cors');
 
-
-
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-
-mongoose.connect('mongodb+srv://admin:0RbiDtVoQ6RwlJZe@cluster0.o2r8tpv.mongodb.net/?appName=Cluster0')
+mongoose.connect('mongodb+srv://romankvas27b_db_user:31CNtTEDtrUO2WWs@cluster0.l2xwaoc.mongodb.net/?appName=Cluster0')
 .then(()=>{
     console.log('Connected to DB');
 })
@@ -21,37 +18,54 @@ mongoose.connect('mongodb+srv://admin:0RbiDtVoQ6RwlJZe@cluster0.o2r8tpv.mongodb.
     console.log(err);
 })
 
-
 const PORT = 3000;
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
-
 const jokeSchema = new mongoose.Schema({
     title: String,
+    likes: Array        
 });
 
 const Joke = mongoose.model('Joke', jokeSchema);
 
+
 app.post('/add-joke', (req, res) => {
     const { title } = req.body;
-    const newJoke = new Joke({ title });
+    const newJoke = new Joke({ title, likes: [] });
     newJoke.save();
-    res.send('Joke added successfully!');   
+    res.send('Joke added successfully!');
 });
+
 
 app.get('/all-jokes', async (req, res) => {
     const jokes = await Joke.find();
     res.send(jokes);
 });
 
+
 app.get('/random-joke', async (req, res) => {
     const jokes = await Joke.find();
     const randomJoke = jokes[Math.floor(Math.random() * jokes.length)];
     res.send(randomJoke);
 });
+
+
+app.delete('/delete-joke/:id', async (req, res) => {
+    await Joke.findByIdAndDelete(req.params.id);
+    res.send('Joke deleted!');
+});
+
+
+app.post('/like-joke/:id', async (req, res) => {
+    const joke = await Joke.findById(req.params.id);
+    joke.likes.push("like"); 
+    joke.save();
+    res.send('Liked!');
+});
+
 app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}`);
 });
